e07565201050725584f5be6b9c41a9605e3c3186
ger619 <abolger254@gmail.com>
2026-08-27 12:09:19 +0300

    test message: refactor event

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return if user.blank?
-
-    if user.has_role?(:admin)
+    user ||= User.new # guest user (not logged in)
+    if user.has_role? :admin
       can :manage, :all
-      return
-    end
 
-    if user.has_role?(:organiser)
+    elsif user.has_role? :organiser
       # Their own events, and everything hanging off them.
       can :manage, Event, user_id: user.id
       can :manage, TicketType, event: { user_id: user.id }
@@ -17,11 +14,12 @@ class Ability
       can %i[read check_in], Ticket, event: { user_id: user.id }
       can :read, User
       can :invite, User
+
+    elsif user.has_role?(:scanner)
+      can :read, Event
+      can %i[read check_in], Ticket
+    else
+      can :read, Event
     end
-
-    return unless user.has_role?(:scanner)
-
-    can :read, Event
-    can %i[read check_in], Ticket
   end
 end
