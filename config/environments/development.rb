f5700965a0ea17e24aae23fc4eec6767b543508c
ger619 <abolger254@gmail.com>
2026-08-31 16:17:49 +0300

    this is life - update sign

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
+  config.public_file_server.enabled = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
