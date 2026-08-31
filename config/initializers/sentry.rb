033d962f799a5d4238ab7faca9dc2dfb9d2cbbc3
ger619 <abolger254@gmail.com>
2026-08-31 16:44:43 +0300

    this is life - update sign

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,26 @@
+Sentry.init do |config|
+  config.dsn = 'https://54a973eba0b07f60398de92866e16de6@o4507601057808384.ingest.de.sentry.io/4512005723455568'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+
+  # Enable sending logs to Sentry
+  config.enable_logs = true
+  # Patch Ruby logger to forward logs
+  config.enabled_patches = [:logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
