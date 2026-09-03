e14d3d432ab09d240556a3a32a5e265a8f86a790
ger619 <abolger254@gmail.com>
2026-09-03 21:27:45 +0300

    All all chanes

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Salato
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.active_storage.variant_processor = :mini_magick
+    config.active_storage.variant_processor = :vips
     config.time_zone = "Nairobi"
 
     # Configuration for the application, engines, and railties goes here.
