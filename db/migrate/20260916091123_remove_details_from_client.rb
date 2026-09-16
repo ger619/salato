675a2a3dad8bbd16aa399f9a2e7f8b465b8c4334
ger619 <abolger254@gmail.com>
2026-09-16 14:14:58 +0300

    Sidekiq fix for all

--- db/migrate/20260916091123_remove_details_from_client.rb
+++ db/migrate/20260916091123_remove_details_from_client.rb
@@ -1,7 +1,7 @@
 class RemoveDetailsFromClient < ActiveRecord::Migration[8.1]
   def change
-    remove_column :clients, :website, :string if exists_column?(:clients, :website)
-    remove_column :clients, :settlement_bank, :string if exists_column?(:clients, :settlement_bank)
-    remove_column :clients, :tax_pin, :string if exists_column?(:clients, :tax_pin)
+    remove_column :clients, :website, :string, if_exists: true
+    remove_column :clients, :settlement_bank, :string, if_exists: true
+    remove_column :clients, :tax_pin, :string, if_exists: true
   end
 end
