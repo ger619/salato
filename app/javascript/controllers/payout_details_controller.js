89045acb357017920db8bc57a9953c734f1a14f7
ger619 <abolger254@gmail.com>
2026-09-16 13:24:27 +0300

    Sidekiq fix for all

--- app/javascript/controllers/payout_details_controller.js
+++ app/javascript/controllers/payout_details_controller.js
@@ -1,22 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Shows the Bank or Mobile money select based on "Payout type".
 // Hidden selects are also disabled, so they are not required and not submitted.
 export default class extends Controller {
-  static targets = ["method", "bank", "mobileMoney"]
+  static targets = ['method', 'bank', 'mobileMoney']
 
   connect() {
-    this.toggle()
+    this.toggle();
   }
 
   toggle() {
-    const method = this.methodTarget.value
-    this.#show(this.bankTarget, method === "bank")
-    this.#show(this.mobileMoneyTarget, method === "mobile_money")
+    const method = this.methodTarget.value;
+    this.#show(this.bankTarget, method === 'bank');
+    this.#show(this.mobileMoneyTarget, method === 'mobile_money');
   }
-
+  // eslint-disable-next-line
   #show(wrapper, visible) {
-    wrapper.hidden = !visible
-    wrapper.querySelectorAll("select, input").forEach((el) => (el.disabled = !visible))
+    wrapper.hidden = !visible;
+    // eslint-disable-next-line
+    wrapper.querySelectorAll('select, input').forEach((el) => (el.disabled = !visible));
   }
 }
